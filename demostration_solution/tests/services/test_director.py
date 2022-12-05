e887c21c3f1cf1c64e7a2f0f032ca26634cbe5c7
lesson17 <molatok@ya.ru>
2022-12-05 18:37:07 +0300

    все тесты для director

--- demostration_solution/tests/services/test_director.py
+++ demostration_solution/tests/services/test_director.py
@@ -12,3 +12,20 @@ class TestDirectorService:
         director = self.director_service.get_one(1)
         assert director is not None
 
+    def test_get_all(self):
+        directors = self.director_service.get_all()
+        assert len(directors) > 1
+
+    def test_create(self):
+        data = {
+            'name': 'TestName4'
+        }
+        director = self.director_service.create(data)
+        assert director.id is not None
+
+    def test_update(self):
+        res = self.director_service.update(1)
+
+    def test_delete(self):
+        res = self.director_service.delete(1)
+        assert res is None
